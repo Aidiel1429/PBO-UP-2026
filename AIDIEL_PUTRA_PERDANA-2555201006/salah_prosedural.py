51e1d1fc799e9bdaf56763f1cf2328cf7d313690
Aidiel Putra Perdana <2555201006@student.universitaspahlawan.ac.id>
2026-09-25 11:31:59 +0700

    P01: struktur repositori dan kelas Mahasiswa

--- AIDIEL_PUTRA_PERDANA-2555201006/salah_prosedural.py
+++
@@ -1,6 +0,0 @@
-nama = "Budi Santoso" 
-nim = "2410123456" 
-desa = "Kuok" 
-def perkenalan(n, m, d):    
-    return "Saya " + n + " (" + m + "), dari " + d + "." 
-print(perkenalan(nama, nim, desa))
